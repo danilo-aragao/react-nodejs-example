ebd825129fb5557060e273e8b143c0ddfc8a203d
Danilo Aragão <danilo.aragao@ctis.com.br>
2020-02-12 14:09:22 -0300

    adding first components

---
+++ frontend/src/components/create-user.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+export default class CreateUser extends Component {
+  render() {
+    return (
+      <div>
+        <p> Create User Component</p>
+      </div>
+    )
+  }
+}
---
+++ frontend/src/components/edit-exercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+export default class EditExercise extends Component {
+  render() {
+    return (
+      <div>
+        <p> Edit Exercise Component</p>
+      </div>
+    )
+  }
+}
